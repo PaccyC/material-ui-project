ea49aa051f7905af6d99e1af65106e1aba060269
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-11 12:04:35 +0200

    created custom button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 
-import { Button } from '@mui/material';
+import { Button, styled } from '@mui/material';
 import './App.css';
 import SettingsIcon from '@mui/icons-material/Settings'
 function App() {
+
+  const BlueButton=styled(Button)({
+    backgroundColor:"skyblue",
+    color:"#888",
+    margin:5,
+    
+  })
   return (
     <div className="App">
-     <Button startIcon={<SettingsIcon/>}  variant='contained' color='secondary'>Contained</Button>
+     <Button 
+        startIcon={<SettingsIcon/>}  
+        variant='contained' 
+        color='secondary'
+        sx={{
+          backgroundColor: 'primary',
+        }}>Contained</Button>
      <Button variant='outlined'>Outlined</Button>
      <Button variant='text'>Text</Button>
+     <BlueButton>One Button</BlueButton>
+     <BlueButton>Another Button</BlueButton>
     </div>
   );
 }
