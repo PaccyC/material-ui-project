f5c76ba43d129ad357af652042021e955af0dd7a
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-11 13:29:50 +0200

    added components setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +11,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
+
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
