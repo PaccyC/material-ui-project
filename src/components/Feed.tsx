c723ab9de6e27e936af196346774e32fb3f0be2d
PaccyC <abayisengaaimepacifique@gmail.com>
2024-07-12 16:55:55 +0200

    finished feed part

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,14 +1,19 @@
-import { Box } from "@mui/material"
+import {  Box } from "@mui/material"
+import Post from "./Post"
 
 const Feed = () => {
   return (
-    <Box sx={{
-        backgroundColor:"blue"
-    }}
+    <Box 
      flex={4}
      p={2}
+   
     >
-      Feed
+      <Post/>
+
+      <Post/>
+      <Post/>
+      <Post/>
+
     </Box>
   )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,21 +1,93 @@
-import { Box } from "@mui/material"
+import { AccountBox, Home, ModeNight, Person, Settings, Storefront } from "@mui/icons-material"
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from "@mui/material"
 
 const Sidebar = () => {
   return (
     <Box 
+
     flex={1}
     p={2}
     sx= {
         {
-            backgroundColor:"yellow",
+            
             display:{
                 xs: 'none',
                 sm: 'block',
             }
         }
     }
+   
     >
-      Sidebar
+      <Box  position="fixed">
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" >
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="HomePage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" >
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" >
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" >
+              <ListItemIcon>
+                <Storefront />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" >
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" >
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" >
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" >
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+             <Switch/>
+            </ListItemButton>
+          </ListItem>
+          </List>
+      </Box>
+
+        
     </Box>
   )
 }
